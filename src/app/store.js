86fb64392f92a077a0005baa6977f66330966f4b
Talha Zulfiqar <csharpjava0@gmail.com>
2022-06-06 17:47:25 +0500

    incomplete persist for stop the redux to going into login page after reload the page

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,22 @@
+import { createStore, applyMiddleware } from "redux";
 import { configureStore } from "@reduxjs/toolkit";
 import useReducer from "../features/userSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
+const persistConfig = {
+  key: "main-root",
+  storage,
+};
 export default configureStore({
   reducer: {
     user: useReducer,
   },
 });
+const persistedReducer = persistReducer(persistConfig, useReducer);
+
+const store = createStore(persistedReducer, applyMiddleware());
+
+const Persistor = persistStore(store);
+
+export { Persistor };
--- src/pages/Addtask.js
+++ src/pages/Addtask.js
@@ -69,19 +69,25 @@ const Addtask = () => {
       </div>
       <div className="flex items-center justify-center mt-[3%]">
         <div className="">
-          {tasks.map((t) => {
-            return (
-              <div key={t}>
-                <p className="sm:w-[500px] xs:w-[300px] bg-brand-3 mb-3 bg-opacity-20 h-11 flex justify-between rounded text-brand-3 font-bold font-poppins p-2.5">
-                  {t.task}
-                  <MdDeleteForever
-                    onClick={() => deleteTask(t._id)}
-                    className="float-right text-red-600 text-[25px] hover:scale-125"
-                  />
-                </p>
-              </div>
-            );
-          })}
+          {tasks.length > 0 ? (
+            tasks.map((t) => {
+              return (
+                <div key={t}>
+                  <p className="sm:w-[500px] xs:w-[300px] bg-brand-3 mb-3 bg-opacity-20 h-11 flex justify-between rounded text-brand-3 font-bold font-poppins p-2.5">
+                    {t.task}
+                    <MdDeleteForever
+                      onClick={() => deleteTask(t._id)}
+                      className="float-right text-red-600 text-[25px] hover:scale-125"
+                    />
+                  </p>
+                </div>
+              );
+            })
+          ) : (
+            <div className="text-brand-3 text-[40px] font-poppins mt-10">
+              No task will be added yet!
+            </div>
+          )}
         </div>
       </div>
     </div>
